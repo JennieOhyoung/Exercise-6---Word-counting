83471f2dbf7d9fdc5ee708d26ba3f063b8cc5589
Hackbright Student <user@chromebox-005.(none)>
2013-09-30 16:42:14 -0700

    Prints words and counts by decreasing count value

--- wordcount.py
+++ wordcount.py
@@ -12,19 +12,31 @@ Returns counts for each time a word occurs in file"""
 
 from sys import argv
 
+def normalize(text):
+    words = text.lower().replace("--", " ")
+    wordList = words.split()
+    return wordList
+
 def main():
     script, filename = argv
     wordcount = {}
 
     open_file = open(filename)
     text = open_file.read()
-    words = text.lower().split()
     open_file.close()
 
-    #splitting by space going line by line
+    words = normalize(text)
 
     for word in words:
+        word = word.strip("_,*&^@+=.!?:;-\"'")
         wordcount[word] = wordcount.get(word, 0) + 1
-        print word, wordcount[word]
+
+    print_count= []
+    for key, value in wordcount.iteritems():
+        print_count.append([value, key])
+    
+    print_count.sort(reverse = True)
+    for value, key in print_count:
+        print key, value
 
 main()
